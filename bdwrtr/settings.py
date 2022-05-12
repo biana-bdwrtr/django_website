53a104eb913564901cab898959565163442ebfbe
bdwrtr <bdwrtr@naver.com>
2022-05-13 02:07:40 +0900

    bootstrap 적용 및 CSS 작업

--- bdwrtr/settings.py
+++ bdwrtr/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS  = [
+    BASE_DIR / "static",
+]
 
 LOGIN_URL = "accounts:login"
 LOGIN_REDIRECT_URL = "public:index"
